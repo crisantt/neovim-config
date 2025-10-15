ebeb0d4a975220c20e47f8ae90b5ca199feb2625
crisantt <jcsanjose@proton.me>
2025-10-15 10:33:50 +0800

    added a new folder and edited the options.lua

---
+++ lua/options.lua
@@ -0,0 +1,15 @@
+local opt = vim.opt
+
+opt.number = true
+opt.cursorline = true
+opt.scrolloff = 4
+opt.sidescrolloff = 8
+opt.tabstop = 2
+opt.autoindent = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.wrap = false
+opt.list = false
+
+opt.ignorecase = true
+opt.hlsearch = false 
