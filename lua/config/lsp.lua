e056c2d46ad1ed43106ec22f85fdd0cb0730be37
crisantt <jcsanjose@proton.me>
2025-10-17 19:29:28 +0800

    colors, telescope, harpoon

---
+++ lua/config/lsp.lua
@@ -0,0 +1,10 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = "LazyFile",
+  dependencies = {
+    "mason.nvim",
+    { "mason-org/mason-lspconfig.nvim", config = function() end },
+	},
+	config = function()
+
+}
